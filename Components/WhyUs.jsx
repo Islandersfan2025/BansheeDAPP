bded7ac4f8e601580e7bd87d6c6bdb4c7c52f127
Lamont <missionpossible23@protonmail.com>
2025-11-20 10:44:49 -0500

    Update WhyUs.jsx

--- Components/WhyUs.jsx
+++ Components/WhyUs.jsx
@@ -4,7 +4,7 @@ import WhyUsDetails from "../Components/Data/WhyUs.json";
 
 const WhyUs = () => {
   return (
-    <div className="uni-whyus uk-section uk-section-xlarge@m uk-panel">
+    <div className="uni-whyus uk-section uk-section-xlarge@m uk-panel" style="text-align: center;">
 
       <h1> Proof-of-Performance </h1>
         <p>Royality model verifies on-chain transactions of artists' sales, views, and concert attendance.</p>
