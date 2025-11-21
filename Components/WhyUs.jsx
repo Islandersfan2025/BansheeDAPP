f1469382ddb629b29ea5159f24b24110693c2597
Lamont <missionpossible23@protonmail.com>
2025-11-20 19:56:35 -0500

    Update WhyUs.jsx

--- Components/WhyUs.jsx
+++ Components/WhyUs.jsx
@@ -5,14 +5,21 @@ import WhyUsDetails from "../Components/Data/WhyUs.json";
 const WhyUs = () => {
   return (
     <div className="uni-whyus uk-section uk-section-xlarge@m uk-panel">
-
-      <h1 style="text-align: center;"> Proof-of-Performance </h1>
-        <p style="text-align: center;">Royality model verifies on-chain transactions of artists' sales, views, and concert attendance.</p>
-
-      <h2 style="text-align: center;">Powered by Subquery</h2>
-      
+      <div className="uk-container">
+        <header
+          className="uk-grid-xsmall uk-flex-center uk-flex-middle uk-grid"
+          data-uk-grid=""
+        >
+          <div className="uk-panel uk-text-center">
+            <h2 className="uk-h3 uk-h1@m">Proof-of-Performance</h2>
+             <p>Royality model that verifies on-chain transactions of artists' sales, views, and concert attendance.</p>
+            <h3>Powered By Subquery</h3>
+          </div>
+        </header>
+      </div>
     </div>
   );
 };
 
 export default WhyUs;
+
